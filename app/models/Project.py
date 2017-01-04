6e9feffad453994c64ac2fc81bf90d0fe6b3c8f5
M G <mg@Ms-MacBook-Pro.local>
2017-01-04 17:06:53 -0500

    created manual queries for booking, project, task, ticket to optimize load time

--- app/models/Project.py
+++ app/models/Project.py
@@ -1,4 +1,5 @@
 from app.models import db, Base
+from sqlalchemy import text
 
 
 class Project(Base):
@@ -16,6 +17,28 @@ class Project(Base):
     finish_date = db.Column(db.Date())
     project_stage_id = db.Column(db.Integer())
     updated = db.Column(db.DateTime())
+
+    @staticmethod
+    def get_my_projects(user_email):
+        query = '''SELECT DISTINCT
+                          p.id,
+                          p.name,
+                          p.active,
+                          p.budget,
+                          p.budget_time,
+                          p.user_id,
+                          p.currency,
+                          p.start_date,
+                          p.finish_date,
+                          p.project_stage_id,
+                          p.updated
+                        FROM project p
+                        LEFT JOIN project_task pt ON pt.project_id = p.id
+                        LEFT JOIN project_task_assign pta ON pta.project_task_id = pt.id
+                        LEFT JOIN booking b ON b.project_id = p.id
+                        LEFT JOIN user u ON (p.user_id = u.id OR pta.user_id = u.id OR b.user_id = u.id)
+                        WHERE u.email = :email AND p.active="1";'''
+        return Project.query.from_statement(text(query)).params(email=user_email).all()
 
     def __repr__(self):
         return '<Project %r>' % self.id
