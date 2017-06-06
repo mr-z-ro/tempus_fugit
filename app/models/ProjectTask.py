f50c1aeeeecb14e38484d12e7386511e1ca3ae78
Christopher Guess <cguess@gmail.com>
2017-06-06 15:54:51 -0500

    fixed some typos with ProjectTask

--- app/models/ProjectTask.py
+++ app/models/ProjectTask.py
@@ -19,17 +19,17 @@ class ProjectTask(Base):
     @staticmethod
     def get_by_id(project_task_id):
         query = '''SELECT DISTINCT
-                      t.id,
-                      t.name,
-                      t.project_id,
+                      pt.id,
+                      pt.name,
+                      pt.project_id,
                       p.name AS project_name,
-                      t.start_date,
-                      t.end_date
+                      pt.start_date,
+                      pt.end_date
                     FROM project_task pt
                     INNER JOIN project p ON pt.project_id = p.id
                     WHERE
                     pt.id = :project_task_id;'''
-        return Task.query.from_statement(text(query)).params(project_task_id=project_task_id).all()
+        return ProjectTask.query.from_statement(text(query)).params(project_task_id=project_task_id).all()
 
 
     def __repr__(self):
